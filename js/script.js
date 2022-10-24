6c2d88648e9936e5bf7809ee49c10010dddbc7c5
MikeyGloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-10-24 13:49:53 -0400

    code change

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mikey Gloriani
+// Created on: Oct 2022
 // This file contains the JS functions for index.html
 
 "use strict"
